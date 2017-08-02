4e93b7a43e050200cf4d9c8c7cd683b92a7476f3
BellZhs <DienBell@gmail.com>
2017-08-02 14:07:43 +0800

    Create basic_auth.js

---
+++ basic_auth.js
@@ -0,0 +1,17 @@
+var http = require('http') 
+var auth = require('basic-auth') 
+  
+// Create server  
+var server = http.createServer(function (req, res) { 
+  var credentials = auth(req) 
+  
+  if (!credentials || credentials.name !== 'bell' || credentials.pass !== 'iloveu') { 
+    res.statusCode = 401 
+    res.setHeader('WWW-Authenticate', 'Basic realm="example"') 
+    res.end('Access denied') 
+  } 
+  res.end('Access granted') 
+}) 
+  
+// Listen  
+server.listen(3000)                     
